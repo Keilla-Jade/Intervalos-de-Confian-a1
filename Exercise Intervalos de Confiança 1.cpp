2fe83f3769227f79efac88188374eb89d7294c2d
Keilla-Jade <80544475+Keilla-Jade@users.noreply.github.com>
2021-07-13 21:39:41 -0300

    Add files via upload

---
+++ Exercise Intervalos de Confiança 1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double erf(double x)
+{  
+	double y = 1.0 / ( 1.0 + 0.3275911 * x);   
+	return 1 - (((((+ 1.061405429  * y - 1.453152027) * y + 1.421413741) * y - 0.284496736) * y + 0.254829592) * y) * exp(-x * x);      
+}
+
+double pdf(double x, double mu, double std_dev)
+{
+	static const double pi = 3.14159265; 
+	return exp( -1 * (x - mu) * (x - mu) / (2 * std_dev * std_dev)) / (std_dev * sqrt(2 * pi));
+}
+
+double cdf(double x, double mu, double std_dev)
+{
+	return 0.5 * (1 + erf((x - mu) / (std_dev * sqrt(2.))));
+}
+
+int main()
+{	
+	double x, mu, std_dev;
+	
+	cout << "Insira os valores, na sequencia, de: x, mu, std_dev: ";
+	cin >> x >> mu >> std_dev;
+
+	cout << "PDF de x e: " << pdf(x, mu, std_dev) << endl;
+	cout << "CDF de x e: " << cdf(x, mu, std_dev) << endl;
+	cout << "CDF of x e (mu = 0, std_dev = 1): " << cdf(x, 0, 1) << endl;
+
+	return 0;
+}
